24a8d286fe8d694ccdada3a5c831f82e6f8ce359
Olga7 test <7enovaoa@gmail.com>
2025-03-08 13:33:37 +0000

    Переписан репозиторий для работы с JPA

--- src/main/java/app/example/springBootHibernate/controller/PersonController.java
+++ src/main/java/app/example/springBootHibernate/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -15,14 +16,31 @@ public class PersonController {
 
     // Конструктор для инжекции PersonRepository
     public PersonController(PersonRepository personRepository) {
+
         this.personRepository = personRepository;
     }
 
-    // Метод-обработчик GET-запроса с маппингом на /persons/by-city
-    @GetMapping("/persons/by-city")
-    public List<Person> getPersonsByCity(@RequestParam String city) {
-        // Передаем city в репозиторий для фильтрации
-        return personRepository.getPersonsByCity(city);
+//    // Метод-обработчик GET-запроса с маппингом на /persons/by-city
+//    @GetMapping("/persons/by-city")
+//    public List<Person> getPersonsByCity(@RequestParam String city) {
+//        // Передаем city в репозиторий для фильтрации
+//        return personRepository.getPersonsByCity(city);
+//    }
+// Метод для получения людей по городу
+@GetMapping("/persons/by-city")
+public List<Person> getPersonsByCity(@RequestParam String city) {
+    return personRepository.findByCityOfLiving(city);
+}
+    // Метод для получения людей по возрасту, меньше заданного возраста, отсортированных по возрастанию
+    @GetMapping("/persons/by-age")
+    public List<Person> getPersonsByAge(@RequestParam Integer age) {
+        return personRepository.findByAgeLessThanOrderByAgeAsc(age);
+    }
+
+    // Метод для получения человека по имени и фамилии
+    @GetMapping("/persons/by-name-and-surname")
+    public Optional<Person> getPersonByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
+        return personRepository.findByNameAndSurname(name, surname);
     }
 }
 
